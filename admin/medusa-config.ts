4f21dc179155e4b84e4062cc4072bcca464f126b
Bui Minh Toan <toan.bui@nexpando.com>
2025-06-21 12:14:03 +0700

    +

--- admin/medusa-config.ts
+++ admin/medusa-config.ts
@@ -12,5 +12,10 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
+  },
+  admin: {
+    serve: true,
+    autoRebuild: false,
+    path: "/admin"
   }
 })
