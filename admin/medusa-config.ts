79478105f3da9b5f8ecb31a7b56932ec7f0118bf
Bui Minh Toan <toan.bui@nexpando.com>
2025-06-22 14:53:35 +0700

    +

--- admin/medusa-config.ts
+++ admin/medusa-config.ts
@@ -5,7 +5,8 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL || "postgres://marketplace_postgres:Vu]Q\"<F^^G2@\\sE3@omnibot.cloud:5433/scripts_marketplace",
-    redisUrl: process.env.REDIS_URL || "redis://localhost:6379",
+    // Disable Redis for now since it's not available on Render
+    // redisUrl: process.env.REDIS_URL || "redis://localhost:6379",
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
